ad339403a0bfdfec9dcd32b30d40a73bc86cd225
knorixx <133777606+knorixx@users.noreply.github.com>
2024-04-20 12:04:23 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+let button = document.querySelector(".button");
+button.addEventListener('click', addNewTask);
+function addNewTask(event) {
+    let nameBtn = String(prompt("Введи имя"));
+    alert('Привет' + '' + nameBtn)
+};
