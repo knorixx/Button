1061096502f872938858176618d5dbbf1ed27b9f
knorixx <133777606+knorixx@users.noreply.github.com>
2024-04-20 13:26:33 +0500

    Update java.js

--- java.js
+++ java.js
@@ -2,5 +2,5 @@ let button = document.querySelector(".button");
 button.addEventListener('click', addNewTask);
 function addNewTask(event) {
     let nameBtn = String(prompt("Введи имя"));
-    alert('Привет' + '' + nameBtn)
+    alert('Привет' + ' ' + nameBtn)
 };
